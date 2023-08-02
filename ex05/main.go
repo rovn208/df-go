4d56e80d8c51bbd922d68287be185dc7065fea44
rovn208 <ngocro208@gmail.com>
2023-08-02 17:14:25 +0200

    refactor(ex05): Add wait group and buffer channels

--- ex05/main.go
+++ ex05/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/gocolly/colly"
-	"log"
 	"strings"
+	"sync"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
 	animes := make([]Anime, 0)
 	urls := make([]string, 0)
-	pageLimit := 3
-	i := 0
+	visited := make([]string, 0)
+	var wg sync.WaitGroup
+	chs := make(chan interface{}, 4)
+	i := 1
+	pageLimit := 40
 
 	c := colly.NewCollector()
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
+	c.OnRequest(func(e *colly.Request) {
+		fmt.Printf("visiting %v\n", e.URL)
+	})
+
 	c.OnHTML("ul.pagination > li:not(.active)", func(e *colly.HTMLElement) {
 		url := getURL(e)
-		if len(url) > 0 && !contains(urls, url) {
+		if len(url) > 0 && !contains(visited, url) {
 			urls = append(urls, url)
 		}
 	})
@@ -32,27 +40,34 @@ func main() {
 		animes = append(animes, anime)
 	})
 
-	c.OnScraped(func(_ *colly.Response) {
-		if len(urls) > 0 && i < pageLimit {
+	c.OnError(func(e *colly.Response, err error) {
+		defer wg.Done()
+		fmt.Printf("Something went wrong when visiting %v: %v\n", e.Request.URL, err)
+		<-chs
+	})
+
+	c.OnScraped(func(e *colly.Response) {
+		defer wg.Done()
+		<-chs
+
+		for len(urls) > 0 && i < pageLimit {
 			url := urls[0]
 			urls = urls[1:]
+			visited = append(visited, url)
 			i++
-			c.Visit(url)
+
+			wg.Add(1)
+			chs <- struct{}{}
+			go c.Visit(url)
 		}
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		fmt.Println("Something went wrong: ", err)
-	})
+	wg.Add(1)
+	chs <- struct{}{}
+	go c.Visit("https://anime47.com/danh-sach/phim-moi/1.html")
+	visited = append(visited, "https://anime47.com/danh-sach/phim-moi/1.html")
+	wg.Wait()
 
-	c.OnRequest(func(e *colly.Request) {
-		fmt.Printf("visiting %v\n", e.URL)
-	})
-
-	err := c.Visit("https://anime47.com/danh-sach/phim-moi/1.html")
-	if err != nil {
-		log.Fatal(err)
-	}
 	fmt.Printf("Anime scraped: %d", len(animes))
 }
 
@@ -66,5 +81,11 @@ func contains(s []string, e string) bool {
 }
 
 func getURL(e *colly.HTMLElement) string {
-	return "https://" + strings.Replace(e.ChildAttr("a", "href"), ".", e.Request.URL.Host, 1)
+	href := e.ChildAttr("a", "href")
+
+	if len(href) > 0 {
+		return strings.Replace(href, ".", fmt.Sprintf("%s%s", "https://", e.Request.URL.Host), 1)
+	}
+
+	return ""
 }
